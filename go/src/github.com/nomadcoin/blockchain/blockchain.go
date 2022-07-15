42866e1acca0a58fafad2300717dd9a699dd04c4
9noeS <leesc332@gmail.com>
2022-07-15 19:00:11 +0900

    REST API

---
+++ go/src/github.com/nomadcoin/blockchain/blockchain.go
@@ -0,0 +1,69 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"sync"
+)
+
+type Block struct {
+	Data     string `json:"data"`
+	Hash     string `json:"hash"`
+	PrevHash string `json:"prevHash,omitempty"`
+	Height   int    `json:"Height"`
+}
+
+type blockchain struct {
+	blocks []*Block
+}
+
+var b *blockchain
+var once sync.Once
+
+func (b *Block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockChain().blocks[totalBlocks-1].Hash
+}
+
+func createBlock(data string) *Block {
+	newBlock := Block{data, "", getLastHash(), len(GetBlockChain().blocks) + 1}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(Data string) {
+	b.blocks = append(b.blocks, createBlock(Data))
+}
+
+func GetBlockChain() *blockchain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis")
+		})
+	}
+	return b
+}
+
+func (b *blockchain) AllBlocks() []*Block {
+	return GetBlockChain().blocks
+}
+
+var ErrNotFound = errors.New("block not found")
+
+func (b *blockchain) GetBlock(height int) (*Block, error) {
+	if height > len(b.blocks) {
+		return nil, ErrNotFound
+	}
+
+	return b.blocks[height-1], nil
+}
